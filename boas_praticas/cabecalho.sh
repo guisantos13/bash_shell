8519338e2366c17d6173b19ff4c71d0b29824fb9
guisantos13 <ghs23.06@hotmail.com>
2023-06-06 20:30:07 -0300

    boas praticas sh

---
+++ boas_praticas/cabecalho.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#
+# cabeçalho.sh      - Script de exemplo do curso "Aprenda Shell Script"
+# Site:             https://www.aprendashellscript.com
+# Autor:            Guilherme Santos
+# Manutenção:       Guilherme Santos
+#
+# ------------------------------------------------------------------------ #
+# Este programa irá mostrar na tela o texto "Olá Mundo!", esperando       
+# que seja digitado o nome de uma pessoa.                                 
+#                                                                         
+#   Exemplos:                                                                
+#      $ ./cabeçalho.sh                                                     
+#      Neste exemplo o script será executado e irá pedir para que seja     
+#      digitado o nome de uma pessoa.                                      
+# ------------------------------------------------------------------------ #    
+# Histórico:
+#
+#   v1.0 06/06/2023, Guilherme:
+#       - Início do programa
+#       - Conta com a funcionalidade X
+#   v1.1 07/07/2023, Guilherme:
+#       - Alterado parametro XXXXX
+# ------------------------------------------------------------------------ #
+# Testado em:
+#   bash 5.1.16
+# ------------------------------------------------------------------------ #
+# Agradecimentos:
+#
+# 	Joãozinho - Encontrou um bug na parte de login.
+#	Mariazinha - Enviou uma sugestão de adicionar o -h.
+# ------------------------------------------------------------------------ #
+
+
+# ------------------------------- VARIÁVEIS ----------------------------------------- #
